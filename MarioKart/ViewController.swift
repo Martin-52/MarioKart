5b56b6f8439f3c480a085e5f6b58c55ca8d2b40d
Martin Landin <martinlandin@martins-macbook-air.local>
2019-02-20 14:08:44 -0800

    Initial Commit

---
+++ MarioKart/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MarioKart
+//
+//  Created by Martin Landin on 2/20/19.
+//  Copyright © 2019 Martin Landin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
